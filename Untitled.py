be9bd2c6bae286172d5330357e605eeef6f01bf9
jordanxthexkid <31998726+jordanxthexkid@users.noreply.github.com>
2018-03-08 19:31:10 -0600

    Working frame
    The frame works, it just doesn't have color

---
+++ Untitled.py
@@ -0,0 +1,3 @@
+fd = open("filename","w")
+input = raw_input("user input")
+fd.write(input)
--- Working_frame_with_color.py
+++ Working_frame_with_color.py
@@ -1,11 +1,10 @@
 import PIL
 import matplotlib.pyplot as plt # single use of plt is commented out
 import os.path  
-import PIL.ImageDraw            
+import PIL.ImageDraw                
 
-def round_corners(original_image, percent_of_side):
-    """ Rounds the corner of a PIL.Image
-    
+def frame(original_image,color,frame_width):
+    """ frames the image with a specified color of a PIL.Image
     original_image must be a PIL.Image
     Returns a new PIL.Image with rounded corners, where
     0 < percent_of_side < 1
@@ -13,48 +12,34 @@ def round_corners(original_image, percent_of_side):
     """
     #set the radius of the rounded corners
     width, height = original_image.size
-    radius = int(percent_of_side * min(width, height)) # radius in pixels
+    thickness = int(frame_width * min(width, height)) # thickness in pixels
     
     ###
     #create a mask
     ###
     
     #start with transparent mask
-    rounded_mask = PIL.Image.new('RGBA', (width, height), (127,0,127,0))
-    drawing_layer = PIL.ImageDraw.Draw(rounded_mask)
+    r,g,b=color
+    frame_mask = PIL.Image.new('RGBA', (width, height), (0,0,0,0))
+    drawing_layer = PIL.ImageDraw.Draw(frame_mask)
     
     # Overwrite the RGBA values with A=255.
     # The 127 for RGB values was used merely for visualizing the mask
-    
     # Draw two rectangles to fill interior with opaqueness
-    drawing_layer.polygon([(radius,0),(width-radius,0),
-                            (width-radius,height),(radius,height)],
-                            fill=(127,0,127,255))
-    drawing_layer.polygon([(0,radius),(width,radius),
-                            (width,height-radius),(0,height-radius)],
-                            fill=(127,0,127,255))
-
-    #Draw four filled circles of opaqueness
-    drawing_layer.ellipse((0,0, 2*radius, 2*radius), 
-                            fill=(0,127,127,255)) #top left
-    drawing_layer.ellipse((width-2*radius, 0, width,2*radius), 
-                            fill=(0,127,127,255)) #top right
-    drawing_layer.ellipse((0,height-2*radius,  2*radius,height), 
-                            fill=(0,127,127,255)) #bottom left
-    drawing_layer.ellipse((width-2*radius, height-2*radius, width, height), 
-                            fill=(0,127,127,255)) #bottom right
-                         
-    # Uncomment the following line to show the mask
-    plt.imshow(rounded_mask)
+    #drawing_layer.rectangle((0,0,width,thickness),fill=(r,g,b,255))
+    #drawing_layer.rectangle((0,height-thickness,width,height),fill=(r,g,b,255))
+    #drawing_layer.rectangle((0,height,thickness,0),fill=(r,g,b,255))
+    #drawing_layer.rectangle((width,0,width-thickness,height),fill=(r,g,b,255))
+    drawing_layer.rectangle((thickness,thickness,width-thickness,height-thickness),fill=(r,g,b,255))
     
     # Make the new image, starting with all transparent
-    result = PIL.Image.new('RGBA', original_image.size, (0,0,0,0))
-    result.paste(original_image, (0,0), mask=rounded_mask)
+    #frame_image = PIL.Image.new('RGBA',(width,height),(r,g,b,255))
+    result = PIL.Image.new('RGBA', original_image.size, (255,0,0,255))
+    result.paste(original_image, (0,0), mask=frame_mask)
     return result
     
 def get_images(directory=None):
     """ Returns PIL.Image objects for all the images in directory.
-    
     If directory is not specified, uses current directory.
     Returns a 2-tuple containing 
     a list with a  PIL.Image object for each image file in root_directory, and
@@ -78,9 +63,8 @@ def get_images(directory=None):
             pass # do nothing with errors tying to open non-images
     return image_list, file_list
 
-def round_corners_of_all_images(directory=None):
+def frame_all_images(color=(255,0,0), directory=None, frame_width= 0.05):
     """ Saves a modfied version of each image in directory.
-    
     Uses current directory if no directory is specified. 
     Places images in subdirectory 'modified', creating it if it does not exist.
     New image files are of type PNG and have transparent rounded corners.
@@ -102,10 +86,11 @@ def round_corners_of_all_images(directory=None):
     #go through the images and save modified versions
     for n in range(len(image_list)):
         # Parse the filename
-        filename, filetype = os.path.splitext(file_list[n])
-        
+        filename, filetype = file_list[n].split('.')
+        print n
         # Round the corners with radius = 30% of short side
-        new_image = round_corners(image_list[n],.30)
+        new_image = frame(image_list[n], color, frame_width)
         #save the altered image, suing PNG to retain transparency
         new_image_filename = os.path.join(new_directory, filename + '.png')
-        new_image.save(new_image_filename)    
+        new_image.save(new_image_filename)
+        
